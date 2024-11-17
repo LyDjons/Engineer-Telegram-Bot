06a2da4cb3196f0383618c76a6c2a2a4a7314902
LyDjons <znahar917@gmail.com>
2024-11-17 17:31:19 +0200

    templates/templates.py  was added

---
+++ WialonLocal/templates/Templates.py
@@ -0,0 +1,11 @@
+
+LOGISTIC_MESSAGE_STATUS = (
+                    "Власні авто   : {count_cluster}"
+                    "Наймані авто  : {count_rental}"
+                    "Інші кластера : {other_clusters}"
+                    " - ЧІМК: {cluster_chimc}"
+                    " - БА  : {cluster_ba}"
+                    " - АК  : {cluster_ak}"
+                    " - СА  : {cluster_sa}"
+                    " - АП  : {cluster_ap}"
+                           )
